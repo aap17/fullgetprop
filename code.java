283d12ce784399471bc164fb6eea952892c06c6b
Grok <no@mail.com>
2017-10-06 17:35:46 +0300

    initial

---
+++ code.java
@@ -0,0 +1,23 @@
+c static String fullSystemProperties(String propName) {
+        String line="";
+        BufferedReader input = null;
+        try {
+            java.lang.Process p = Runtime.getRuntime().exec("getprop " + propName);
+            input = new BufferedReader(new InputStreamReader(p.getInputStream()), 1024);
+            while (input.readLine()!=null) {
+                line = line + "\n" + input.readLine();
+            }
+            input.close();
+        } catch (IOException ex) {
+            return null;
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return line;
+    }
